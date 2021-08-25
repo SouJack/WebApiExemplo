cd386e4093b42d718fd40ff09000d9415cafaffb
Jack <soujack@hotmail.com>
2021-08-25 11:08:11 -0300

    Acrescentando consultar um servico externo sem tratamento

---
+++ Vibe.Exemplo.WebApi/Controllers/ConsultarCepController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vibe.Exemplo.WebApi.Servicos;
+
+namespace Vibe.Exemplo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultarCepController : ControllerBase
+    {
+        public IConsultaCepServico ConsultaCepServico { get; }
+        public ConsultarCepController(IConsultaCepServico consultaCepServico)
+        {
+            ConsultaCepServico = consultaCepServico;
+        }
+
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> Consultar(string cep)
+        {
+            var r = await ConsultaCepServico.Consultar(cep);
+            return Ok(r);
+        }
+    }
+}
---
+++ Vibe.Exemplo.WebApi/Servicos/IConsultaCepServico.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Refit;
+using Vibe.Exemplo.WebApi.Modelos;
+
+namespace Vibe.Exemplo.WebApi.Servicos
+{
+    public interface IConsultaCepServico
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<CepResultado> Consultar(string cep);
+    }
+}
